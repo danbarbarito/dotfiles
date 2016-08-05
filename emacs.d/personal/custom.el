a2f48e3f922a81d3bffd0ddfec14bdfd812bdac0
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-08-05 03:01:16 -0400

    fixed custom.el

--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -3,21 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(global-rbenv-mode t)
+ '(custom-enabled-themes (quote (misterioso)))
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
- '(helm-ag-insert-at-point (quote symbol))
- '(org-mobile-directory "~/org")
+ '(helm-ag-insert-at-point (quote symbol)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#ffcc00" :foreground "black"))))
+ '(cursor ((t (:background "salmon1"))))
+ '(mode-line ((t (:background "#212931" :foreground "#eeeeec" :height 0.85))))
  '(powerline-active1 ((t (:background "grey22" :foreground "white" :inherit mode-line))))
  '(powerline-active2 ((t (:background "grey40" :foreground "white" :inherit mode-line)))))
