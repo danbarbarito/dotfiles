c3af1658592e6d56ef6717271f32eac5badb20f3
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-08-18 03:15:39 -0400

    fsdflsdjlfkdsjf

--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -6,13 +6,16 @@
  '(custom-enabled-themes (quote (misterioso)))
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
- '(helm-ag-insert-at-point (quote symbol)))
+ '(helm-ag-insert-at-point (quote symbol))
+ '(helm-ff-guess-ffap-filenames nil)
+ '(helm-ff-guess-ffap-urls t)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "salmon1"))))
- '(mode-line ((t (:background "#212931" :foreground "#eeeeec" :height 0.85))))
+ '(mode-line ((t (:background "#ffcc00" :foreground "black"))))
  '(powerline-active1 ((t (:background "grey22" :foreground "white" :inherit mode-line))))
  '(powerline-active2 ((t (:background "grey40" :foreground "white" :inherit mode-line)))))
