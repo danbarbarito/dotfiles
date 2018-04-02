30306bee687daacb3b7aebfb923650672d8e4cfe
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-04-02 15:47:23 -0400

    dotfiles-sync commit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -107,6 +107,8 @@
      beginend
      projectile
      ag
+     web-mode
+     php-mode
      )))
 
 (condition-case nil
@@ -249,3 +251,19 @@
 ;; Projectile
 (require 'projectile)
 (projectile-global-mode)
+
+;; PHP Mode
+(require 'php-mode)
+
+;; Web Mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+
--- emacs.d/settings/key-bindings.el
+++ emacs.d/settings/key-bindings.el
@@ -69,10 +69,6 @@
 ;; Turn on the menu bar for exploring new modes
 (global-set-key (kbd "C-<f10>") 'menu-bar-mode)
 
-;; Use shell-like backspace C-h, rebind help to F1
-(define-key key-translation-map [?\C-h] [?\C-?])
-(global-set-key (kbd "<f1>") 'help-command)
-
 (global-set-key (kbd "M-h") 'kill-region-or-backward-word)
 
 ;; Transpose stuff with M-t
