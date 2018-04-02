0eef4a1f29bfab7462e406190fbf7be9da91cd30
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-04-02 14:57:55 -0400

    dotfiles-sync commit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -246,6 +246,6 @@
 (when (file-exists-p user-settings-dir)
   (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
 
-;; Projectilep
+;; Projectile
 (require 'projectile)
 (projectile-global-mode)
