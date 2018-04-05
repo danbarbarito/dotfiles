76fec13b2be33da14af2a5e9d48c4df714119612
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-04-05 10:00:41 -0400

    dotfiles-sync commit

--- emacs.d/settings/setup-org.el
+++ emacs.d/settings/setup-org.el
@@ -11,7 +11,7 @@
 (defadvice kill-whole-line (after fix-cookies activate)
   (myorg-update-parent-cookie))
 
-(setq org-directory "~/Dropbox/org")
+(setq org-directory "~/Box\ Sync/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (define-key global-map (kbd "M-<f6>") 'org-capture)
 
