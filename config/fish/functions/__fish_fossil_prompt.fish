24a754d75c829a0cb4aba8cc061a5f3ea0a33e4e
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-07-25 19:19:33 -0400

    Fix fossil fish script

--- config/fish/functions/__fish_fossil_prompt.fish
+++ config/fish/functions/__fish_fossil_prompt.fish
@@ -1,11 +1,13 @@
 function  __fish_fossil_prompt --description "Prompt function for Fossil"
     set -l separator "|"
     set -l info ""
+    set -l merge_info ""
     set -l branch (command fossil branch ls 2>/dev/null | awk '/\* /{print $2}')
 
     if test -n "$branch"
         set -l stash (command fossil stash list 2>/dev/null)
         set -l changes (command fossil changes 2>/dev/null | awk '{print $1}')
+        set -l info_command (command fossil info 2>/dev/null | awk '/child: /{print $2}')
         set -l extra (count (command fossil extra 2>/dev/null))
         
         if test "$stash" != 'empty stash'
@@ -14,7 +16,7 @@ function  __fish_fossil_prompt --description "Prompt function for Fossil"
 
         if test -n "$changes"
             if contains 'MERGED_WITH' $changes
-                set info $gray$separator$yellow"MERGING!"
+                set merge_info $separator"MERGING!"
                 set extra 0
             end
             set col magenta
@@ -25,7 +27,16 @@ function  __fish_fossil_prompt --description "Prompt function for Fossil"
         if test $extra -ne 0
             set info "$separator+$extra"
         end
+
+        # if contains "child" $info_command
+        #     set branch "not on latest commit on $branch"
+        # end
+
+        if test "$info_command" != ''
+            set info_command (string sub -s 1 -l 5 $info_command)
+            set branch "$info_command | $branch"
+        end
         
-        set_color normal; echo -n " on "; set_color $col; echo -n "$branch$info"; set_color normal;
+        set_color normal; echo -n " on "; set_color $col; echo -n "$branch$info"; set_color red; echo -n "$merge_info"; set_color normal;
     end
 end
