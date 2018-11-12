0bc907bae63565e34a4b7f8e520034785dddd037
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-11-12 09:42:39 -0500

    flycheck config

--- emacs.d/lisp/init-flycheck.el
+++ emacs.d/lisp/init-flycheck.el
@@ -3,7 +3,11 @@
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
   (when (maybe-require-package 'flycheck-color-mode-line)
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+
+
+  '(flycheck-clang-language-standard "c++14")
+  '(flycheck-clang-include-path (quote ("thirdparty/"))))
 
 
 (provide 'init-flycheck)
