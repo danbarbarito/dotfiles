caee08d016230458c285208f9fb91706e3dfe7a8
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-11-21 01:24:45 -0500

    fix perl indent

--- emacs.d/lisp/init-perl.el
+++ emacs.d/lisp/init-perl.el
@@ -2,4 +2,11 @@
 
 (defalias 'perl-mode 'cperl-mode)
 
+(setq
+ cperl-close-paren-offset -4
+ cperl-continued-statement-offset 4
+ cperl-indent-level 4
+ cperl-indent-parens-as-block t
+ cperl-tabs-always-indent t)
+
 (provide 'init-perl)
