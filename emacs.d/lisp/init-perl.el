9bd1ff4a055805201dc40c7b7c1b472cbce8de0e
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-11-21 01:32:04 -0500

    fix perl indent

--- emacs.d/lisp/init-perl.el
+++ emacs.d/lisp/init-perl.el
@@ -7,6 +7,12 @@
  cperl-continued-statement-offset 4
  cperl-indent-level 4
  cperl-indent-parens-as-block t
- cperl-tabs-always-indent t)
+ cperl-tabs-always-indent t )
+
+(defadvice cperl-backward-to-start-of-continued-exp (after indentation-fix)
+  (and (not (memq char-after '(?\) ?\})))
+       (or (not is-block) (looking-back "^[ \t]+"))
+       (> (current-column) cperl-continued-statement-offset)
+       (backward-char cperl-continued-statement-offset)))
 
 (provide 'init-perl)
