00ff2168f6fcd53050a74e45852bc32cfe158ec8
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2018-11-24 15:32:55 -0500

    add hbs extension for web mode

--- emacs.d/lisp/init-web.el
+++ emacs.d/lisp/init-web.el
@@ -7,6 +7,7 @@
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tt\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
 
 (provide 'init-web)
