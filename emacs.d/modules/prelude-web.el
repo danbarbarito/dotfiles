9170a9fc8783d7f778d428e3a932e2c1876a30af
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-03-02 13:18:16 -0500

    add vue to web-mode config

--- emacs.d/modules/prelude-web.el
+++ emacs.d/modules/prelude-web.el
@@ -45,6 +45,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html.ep\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist
 '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
