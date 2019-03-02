c7893baa5c490a92695f75b8ff4990e672af3451
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-03-02 17:16:31 -0500

    remove logs

--- emacs.d/personal/erc.el
+++ emacs.d/personal/erc.el
@@ -1,4 +1,3 @@
 (setq erc-nick "Fricks")
 (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#org-mode"
                                      "#perl" "#git")))
-(setq erc-log-channels-directory "~/.erc/logs/")
