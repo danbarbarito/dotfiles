15f7ac289228b8212e57f3a40804f061079b0eac
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-03-02 17:15:46 -0500

    erc updates

--- emacs.d/personal/erc.el
+++ emacs.d/personal/erc.el
@@ -1,1 +1,4 @@
 (setq erc-nick "Fricks")
+(setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#org-mode"
+                                     "#perl" "#git")))
+(setq erc-log-channels-directory "~/.erc/logs/")
