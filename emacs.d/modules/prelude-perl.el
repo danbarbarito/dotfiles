e0701b448eba745d3a50d6d061423a57633a8613
Dan Barbarito <danbarbarito@ibm.com>
2019-03-12 06:31:10 -0400

    fix close paren offset

--- emacs.d/modules/prelude-perl.el
+++ emacs.d/modules/prelude-perl.el
@@ -53,6 +53,7 @@
   (setq cperl-clobber-lisp-bindings t)
   (setq cperl-lazy-help-time 3)
   (setq cperl-indent-parens-as-block t)
+  (setq cperl-close-paren-offset -4)
 
   ;; if you want all the bells and whistles
   ;; (setq cperl-hairy)
