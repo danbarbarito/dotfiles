3ac9ead7edf2e373eb8130b97db3496d6b294be5
Dan Barbarito <danbarbarito@ibm.com>
2019-03-12 11:06:31 -0400

    add sql to org babel

--- emacs.d/personal/org.el
+++ emacs.d/personal/org.el
@@ -3,3 +3,7 @@
 ;; Go to Org Directory
 (global-set-key (kbd "C-c c o") (lambda() (interactive) (dired org-directory)))
 
+;; active Babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sql . t)))
