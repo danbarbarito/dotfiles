a31ba7b6b5ddadcd14bd8cab43870e43477738a8
Dan Barbarito <danbarbarito@ibm.com>
2019-03-18 16:18:01 -0400

    re-enable electric indent

--- emacs.d/personal/perl.el
+++ emacs.d/personal/perl.el
@@ -1,3 +1,3 @@
-(defun perl-mode-disable-auto-indent()
-  (electric-indent-mode -1))
-(add-hook 'cperl-mode-hook 'perl-mode-disable-auto-indent)
+;; (defun perl-mode-disable-auto-indent()
+;;   (electric-indent-mode -1))
+;; (add-hook 'cperl-mode-hook 'perl-mode-disable-auto-indent)
