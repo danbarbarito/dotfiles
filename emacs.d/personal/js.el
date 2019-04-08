8630cf924e5b0246dc395ecb1fc557fe37d08825
Dan Barbarito <dan@barbarito.me>
2019-04-07 22:32:08 -0400

    disable eslint on save

--- emacs.d/personal/js.el
+++ emacs.d/personal/js.el
@@ -2,6 +2,6 @@
 
 (setq js2-basic-offset 2)
 
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (add-hook 'after-save-hook #'eslint-fix-file-and-revert nil t)))
+;; (add-hook 'js2-mode-hook
+;;           (lambda ()
+;;             (add-hook 'after-save-hook #'eslint-fix-file-and-revert nil t)))
