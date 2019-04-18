e282f6fa013ae153f8272cefa3277ce00d5f39d6
Dan Barbarito <danbarbarito@ibm.com>
2019-04-18 11:17:04 -0400

    perl mode

--- emacs.d/personal/perl.el
+++ emacs.d/personal/perl.el
@@ -1,7 +1,9 @@
 (require 'perltidy)
 
-(setq cperl-indent-level 2)
+(defun my-perl-hook ()
+  (setq cperl-indent-level 2)
+  (add-hook 'before-save-hook #'perltidy-buffer nil t))
 
-(add-hook 'cperl-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'perltidy-buffer nil t)))
+(setq prelude-cperl-mode-hook '(prelude-cperl-mode-defaults
+                                my-perl-hook
+                                ))
