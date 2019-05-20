777b43baa1f2636d15115b835179fec3dcecef0f
Dan Barbarito <dan@barbarito.me>
2019-05-19 23:58:58 -0400

    ledger mode

--- emacs.d/personal/ledger.el
+++ emacs.d/personal/ledger.el
@@ -1,5 +1,4 @@
-(require 'pcomplete)
-(require 'ledger-mode)
-
-;; To open files with .journal extension in hledger-mode
-(add-to-list 'auto-mode-alist '("\\.journal\\'" . ledger-mode))
+(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
+(add-to-list 'load-path
+             (expand-file-name "/path/to/ledger/source/lisp/"))
+(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
