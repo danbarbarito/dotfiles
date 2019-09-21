7ac15ec56d99adbe60defc906b2e5cacee89193f
Dan Barbarito <dan@barbarito.me>
2019-09-21 11:00:59 -0400

    cpp mode

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -55,6 +55,7 @@ values."
      prettier
      vue
      treemacs
+     c-c++
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
