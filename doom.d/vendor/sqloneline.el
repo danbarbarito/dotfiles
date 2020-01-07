bf5f9d5999eed94a30e359291506cd91712f1e4e
Dan Barbarito <danbarbarito@ibm.com>
2020-01-07 11:30:06 -0600

    sqloneline

---
+++ doom.d/vendor/sqloneline.el
@@ -0,0 +1,17 @@
+;;; sqloneline.el --- Turn SQL statement into one line
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+;;   (require 'sqloneline)
+
+;;; Code:
+
+
+;; TODO Finish code
+(defun sqloneline ()
+  "Call sqloneline for whole buffer."
+  (interactive)
+  (goto-char 0))
+
+
+(provide 'sqloneline)
+;;; sqloneline.el ends here
